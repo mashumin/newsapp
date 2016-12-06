edfa8a588161a5fae68f4df00aceb438ae6bb2c9
msm <1345241789@qq.com>
2016-12-06 10:32:46 +0800

    zui new

--- js/index.js
+++ js/index.js
@@ -1,16 +1,19 @@
-function size(origin,type){
-    var type=type || "x";
-    if(type=="x"){
-        var widths=document.documentElement.clientWidth;
-        var sizes=widths/origin*100;
-    }else if(type=="y"){
-        var heights=document.documentElement.clientHeight;
-        var sizes=heights/origin*100;
-    }
-    document.getElementsByTagName("html")[0].style.fontSize=sizes+"px";
-
-}
-window.onload=function(){
-    size(750)
-    alert(1)
-}
+//function size(origin,type){
+//  var type=type || "x";
+//  if(type=="x"){
+//      var widths=document.documentElement.clientWidth;
+//      var sizes=widths/origin*100;
+//  }else if(type=="y"){
+//      var heights=document.documentElement.clientHeight;
+//      var sizes=heights/origin*100;
+//  }
+//  document.getElementsByTagName("html")[0].style.fontSize=sizes+"px";
+//
+//}
+//window.onload=function(){
+//  size(750)
+//  alert(1)
+//}
+$(function(){
+	alert(1)
+})
